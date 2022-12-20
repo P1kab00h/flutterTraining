ef99ea3cd69328c5af42cde116aed9bd6f9e82e5
P1kab00H <nicolas.lefaure@gmail.com>
2022-12-20 14:40:58 +0100

    first commit

---
+++ lib/models/habitation.dart
@@ -0,0 +1,15 @@
+import 'package:location/models/typeHabitat.dart';
+
+class Habitation {
+  int id;
+  TypeHabitat typeHabitat;
+  String image;
+  String libelle;
+  String adresse;
+  int nbpersonnes;
+  int chambres;
+  int superficie;
+  double prixmois;
+
+  Habitation(this.id, this.typeHabitat, this.image, this.libelle, this.adresse, this.nbpersonnes, this.chambres, this.superficie, this.prixmois);
+}
---
+++ lib/views/habitation_details.dart
@@ -0,0 +1,13 @@
+class HabitationDetails extends StatefulWidget {
+  const HabitationDetails({Key? key}) : super(key: key);
+
+  @override
+  State<HabitationDetails> createState() => _HabitationDetailsState();
+}
+
+class _HabitationDetailsState extends State<HabitationDetails> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
